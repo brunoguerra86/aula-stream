2c212571965e53c85ca8d4d87a22b6adf1ae860f
brunoguerra <brunoguerra@hotmail.com>
2023-11-25 17:37:38 -0300

    feat: aprendendo min, max e match

---
+++ src/main/java/AprendendoMinMax.java
@@ -0,0 +1,23 @@
+import java.util.stream.Stream;
+
+public class AprendendoMinMax {
+    public static void main(String[] args) {
+        Double[] notas = {5.5, 5.5, 7.3, 7.2};
+
+        var max = Stream.of(notas).max((nota1, nota2) -> {
+            if(nota1 < nota2) return -1;
+            if(nota1 > nota2) return 1;
+            return 0;
+        }).get();
+
+        System.out.println("Maior nota: " + max);
+
+        var min = Stream.of(notas).min((nota1, nota2) -> {
+            if(nota1 < nota2) return -1;
+            if(nota1 > nota2) return 1;
+            return 0;
+        }).get();
+
+        System.out.println("Menor nota: " + min);
+    }
+}
